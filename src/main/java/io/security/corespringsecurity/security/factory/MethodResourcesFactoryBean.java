af1d297fd6ba3d993e7e6e8418ca1a5acd4c5435
yongtaek12 <laundryday085@gmail.com>
2024-09-20 13:54:46 +0900

    6) AOP Method 기반 DB 연동 - MapBasedSecurityMetadataSource (3)

---
+++ src/main/java/io/security/corespringsecurity/security/factory/MethodResourcesFactoryBean.java
@@ -0,0 +1,49 @@
+package io.security.corespringsecurity.security.factory;
+
+import io.security.corespringsecurity.security.service.SecurityResourceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.access.ConfigAttribute;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@Slf4j
+public class MethodResourcesFactoryBean implements FactoryBean<LinkedHashMap<String, List<ConfigAttribute>>> {
+
+    private SecurityResourceService securityResourceService;
+
+    private String resourcesType;
+    private LinkedHashMap<String, List<ConfigAttribute>> resourcesMap;
+
+    public void setSecurityResourceService(SecurityResourceService securityResourceService) {
+        this.securityResourceService = securityResourceService;
+    }
+
+    public void setResourcesType(String resourcesType) {
+        this.resourcesType = resourcesType;
+    }
+    public void init(){
+//        if("method".equals(resourcesType)){
+            resourcesMap = securityResourceService.getMethodResourceList();
+//        }
+    }
+
+    @Override
+    public LinkedHashMap<String, List<ConfigAttribute>> getObject(){
+        if(resourcesMap ==null){
+            init();
+        }
+        return resourcesMap;
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public Class<LinkedHashMap> getObjectType() {
+        return LinkedHashMap.class;
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+}
